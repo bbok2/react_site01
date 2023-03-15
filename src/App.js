2a3583ae4e9564c2e24e16797213db29e57c309e
y13564@gmail.com <y13564@gmail.com>
2023-03-15 11:18:00 +0900

    230315_1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './Common.css';
 import './App.css';
+import { useState } from 'react';
 
 // JSX에서 class는 사용하지 않고 className을 사용한다
 // tabindex는 tabIndex로 사용한다.
@@ -8,6 +9,15 @@ import './App.css';
 // 이벤트를 가진 컴포넌트 만들기
 
 function FooterMenu(props) {
+  
+  let menu = '브랜드';
+  // var, let, const 변수선언
+  let [menu2, b] = useState('남자코트'); //state는 자동 재랜더링
+  let num = [1, 2];
+  let a = num[0];
+  let [c, d] = [1, 2];
+  let[따봉, 변경] = useState(0);
+
   return (
     <div className="f_menu">
       <ul className="f_menulist">
@@ -15,27 +25,14 @@ function FooterMenu(props) {
           <a href="/" onClick={(event) => {
             event.preventDefault();
             props.onChangeMode();
-          }} 
-          className="f_menuanchor">브랜드</a>
+          }}
+          className="f_menuanchor">{ menu }</a>
         </li>
-        <li className="f_menuitem">
-          <a href="/product/kitchen" className="f_menuanchor">제품</a>
-        </li>
-      </ul>
-      <ul className="f_menulist">
-        <li className="f_menuitem">
-          <a href="/recruitAgency" className="f_menuanchor">대리점 개설문의</a>
-        </li>
-        <li className="f_menuitem">
-          <a href="/customer" className="f_menuanchor">고객센터</a>
-        </li>
-      </ul>
-      <ul className="f_menulist f_menulist--link">
-        <li className="f_menuitem">
-          <a href="https://blog.naver.com/younglim1984" className="f_menuanchor" target="_blank" title="새창 열림">Blog</a>
-        </li>
-        <li className="f_menuitem">
-          <a href="https://www.instagram.com/younglim_official/" className="f_menuanchor" target="_blank" title="새창 열림">Instagram</a>
+        <li className="f_menuitem" onClick={()=>{           
+          변경( 따봉+1 ); 
+          }}>
+          { 따봉 };
+          <span>{ menu2 }</span>
         </li>
       </ul>
     </div>
@@ -43,47 +40,19 @@ function FooterMenu(props) {
 }
 
 function FooterCorp() {
+  let [title, title_m] = useState(['영림임업(주)', '영림화학(주)', '영림산업(주)']);
+
   return (
-    <div className="f_corporaterow">
-      <div className="f_corporate">
-        <div className="f_corporatename">영림임업(주)</div>
-        <div className="f_corporateinfo">인천광역시 남동구 호구포로 9</div>
-        <div className="f_corporateinfo">
-          <div className="f_corporateinfoitem">대표. 김장원</div>          
-        </div>        
-      </div>
-      <div className="f_corporate">
-        <div className="f_corporatename">영림화학(주)</div>
-        <div className="f_corporateinfo">
-          인천광역시 서구 북항로 31번길 13
-        </div>
-        <div className="f_corporateinfo">
-          <div className="f_corporateinfoitem">대표. 황복현</div>
-        </div>
-        <div className="f_corporateinfo">
-          <div className="f_corporateinfoitem f_corporateinfoitem--block">
-            Tel. 1577-5600
-          </div>
-        </div>
-      </div>
-      <div className="f_corporate">
-        <div className="f_corporatename">영림산업(주)</div>
-        <div className="f_corporateinfo">
-          인천광역시 남동구 앵고개로 584
-        </div>
-        <div className="f_corporateinfo">
-          <div className="f_corporateinfoitem">대표. 황복현</div>
-          <div className="f_corporateinfoitem">
-            사업자 등록번호. 131-86-10859
-          </div>
-        </div>
-        <div className="f_corporateinfo">
-          <div className="f_corporateinfoitem f_corporateinfoitem--block">
-            Tel. 1577-5600
-          </div>
-        </div>
-      </div>
-    </div>
+    // div는 최상위 요소는 하나로만 감싼다.    
+    <>
+        <div onClick={()=>{
+          let copy = [...title];
+          copy[0] = '영림공업(쥬)'
+          title_m(copy);
+          }}>{ title[0] }</div>  
+        <div>{ title[1] }</div>
+        <div>{ title[2] }</div>
+    </>
   )
 }
 
@@ -99,14 +68,6 @@ function App() {
             }} />
           </div>
           <FooterCorp />
-        </div>
-      </div>
-      <div className="f_bottom">
-        <div className="f_inner">
-          <div className="f_copy">Younglim</div>
-          <div className="f_cs">
-            고객센터<span className="f_csnumber">1577 - 5600</span>
-          </div>
         </div>
       </div>
     </footer>
